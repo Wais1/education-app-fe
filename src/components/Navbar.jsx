f048f01692f6e0acf42cdb522328355444deebbf
Wais1 <waisibrahim77@gmail.com>
2022-04-11 09:47:29 +0700

    Removed navbar shadow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar(props) {
       className={
         (props.transparent
           ? "top-0 absolute z-50 w-full"
-          : "relative shadow-lg bg-white shadow-lg") +
+          : "relative shadow-sm bg-white shadow-lg") +
         " flex flex-wrap items-center justify-between px-2 py-3 "
       }
     >
